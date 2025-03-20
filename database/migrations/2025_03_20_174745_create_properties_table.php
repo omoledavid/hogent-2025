a66133de0f19e3b5f5eeffac3a14387ef485c890
Omole kessiena David <edokadavid@gmail.com>
2025-03-20 21:00:58 +0100

    Update changes

--- database/migrations/2025_03_20_174745_create_properties_table.php
+++ database/migrations/2025_03_20_174745_create_properties_table.php
@@ -17,14 +17,14 @@ return new class extends Migration
             $table->string('slug')->unique()->nullable();
             $table->text('description')->nullable();
             $table->decimal('price', 10, 2);
-            $table->foreignId('location_id')->constrained('locations')->onDelete('cascade');
+            $table->foreignId('location_id')->nullable();
             $table->foreignId('property_type_id');
             $table->integer('bedrooms')->nullable();
             $table->integer('bathrooms')->nullable();
             $table->integer('size')->nullable();
             $table->enum('status', ['pending','approved', 'sold', 'rented'])->default('pending');
             $table->boolean('is_featured')->default(false);
-            $table->foreignId('user_id')->constrained('user')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->timestamp('deleted_at')->nullable();
             $table->timestamps();
         });
